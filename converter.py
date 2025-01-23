8ae59374473bfc3d0a19dc07d81991b0f05bb81f
Ricardo Bañuelos De La Torre <47264662+RicardoBanuelos@users.noreply.github.com>
2025-01-22 19:42:25 -0800

    conversions will have independent endpoints

--- converter.py
+++ converter.py
@@ -1,5 +1,7 @@
 from docx2pdf import convert
 
+import pandas as pd
+
 UPLOAD_FOLDER = "uploads/"
 CONVERTED_FOLDER = "converted/"
 
@@ -7,7 +9,12 @@ def docx_to_pdf(docx_path: str, pdf_path: str):
     convert(docx_path, pdf_path)
 
 def xlsx_to_csv(xlsx_path: str, csv_path: str):
-    pass
+    df = pd.read_excel(xlsx_path, sheet_name=None)
+
+    for sheet, data in df.items():
+        data.to_csv(f"{csv_path}/{sheet}.csv", index=False)
+
+
 
 
 
--- main.py
+++ main.py
@@ -21,23 +21,33 @@ ALLOWED_EXTENSIONS = {'.docx', '.xlsx'}
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 os.makedirs(CONVERTED_FOLDER, exist_ok=True)
 
-conversion_dict = {
-    ".docx": {
-        ".pdf": docx_to_pdf
-    }
+supported_conversion_dict = {
+    ".docx": {".pdf"},
+    ".xlsx": {".csv"}
 }
 
 def get_file_extension(filename: str):
     split_tup = os.path.splitext(filename)
     return split_tup[1]
 
-def file_is_valid(filename: str) -> bool:
-    return get_file_extension(filename) in ALLOWED_EXTENSIONS
+def validate_file(filename: str) -> bool:
+    if get_file_extension(filename) not in ALLOWED_EXTENSIONS:
+        raise HTTPException(status_code=400, detail="Invalid file type")
+
+def validate_file_exists(file_path) -> bool: 
+    if not os.path.exists(file_path):
+        raise HTTPException(status_code=404, detail="File not found")
+
+def validate_conversion(initial_format: str, target_format: str):
+    if initial_format not in supported_conversion_dict:
+        raise HTTPException(status_code=500, detail="Invalid file found")
+    
+    if target_format not in supported_conversion_dict[initial_format]:
+        raise HTTPException(status_code=400, detail="Unsupported conversion type")
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
-    if not file_is_valid(file.filename):    
-        raise HTTPException(status_code=400, detail="Invalid file type")
+    validate_file(file.filename)
     
     file_id = str(uuid4())
     file_extension = get_file_extension(file.filename)
@@ -50,25 +60,21 @@ async def upload_file(file: UploadFile = File(...)):
         "filename" : f"{file_id}{file_extension}"
     }
 
-@app.get("/convert")
-async def convert_file(filename: str, target_format: str):
-    file_path = os.path.join(UPLOAD_FOLDER, filename)
+@app.get("/convert_docx_to_pdf")
+async def convert_docx_to_pdf(filename: str):
+    file_path = os.path.join(filename)
 
-    if not os.path.exists(file_path):
-        raise HTTPException(status_code=404, detail="File not found")
+    validate_file_exists(file_path)
     
+    TARGET_FORMAT = ".pdf"
+
     file_extension = get_file_extension(filename)
+    validate_conversion(file_extension, TARGET_FORMAT)
 
-    if file_extension not in conversion_dict:
-        raise HTTPException(status_code=500, detail="Invalid file found")
-    
-    if target_format not in conversion_dict[file_extension]:
-        raise HTTPException(status_code=400, detail="Unsupported conversion type")
-    
-    output_filename =  f"{str(uuid4())}{target_format}"
+    output_filename =  f"{str(uuid4())}{TARGET_FORMAT}"
     output_path = os.path.join(CONVERTED_FOLDER, output_filename)
 
-    conversion_dict[file_extension][target_format](file_path, output_path)
+    docx_to_pdf(file_path, output_path)
 
     return {
         "filename" : output_filename
