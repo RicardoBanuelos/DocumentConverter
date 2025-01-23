2e92896fb9f18654334b1a6036255f0ebde9b6e0
Ricardo Bañuelos De La Torre <47264662+RicardoBanuelos@users.noreply.github.com>
2025-01-22 18:42:48 -0800

    switch to windows to use docx2pdf
    docx2pdf is so much better than pandoc

--- converter.py
+++ converter.py
@@ -1,11 +1,10 @@
-import subprocess
+from docx2pdf import convert
 
 UPLOAD_FOLDER = "uploads/"
 CONVERTED_FOLDER = "converted/"
 
 def docx_to_pdf(docx_path: str, pdf_path: str):
-    subprocess.run(["pandoc", docx_path, "-o", pdf_path, "--pdf-engine=lualatex",
-    "-V", "geometry:margin=1in"])
+    convert(docx_path, pdf_path)
 
 def xlsx_to_csv(xlsx_path: str, csv_path: str):
     pass
